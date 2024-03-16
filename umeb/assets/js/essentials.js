b1911d397c83891243ab4441c501036a0cae824e
Megan Miller <megmil@umich.edu>
2024-03-16 15:30:25 -0400

    added heros for every page but contact

--- umeb/assets/js/essentials.js
+++ umeb/assets/js/essentials.js
@@ -1,34 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loadHeader();
+    loadFooter();
+    loadHeros();
+});
+
 /*********************
 SITE HEADER
 *********************/
 
-document.getElementById("umeb-header").innerHTML =
-"<div class='wrapper'>"
-    + "<a class='site-title' href='/'>"
-        + "<span class='site-logo'>"
-            + "<img src='/assets/img/logo.png'>"
-        + "</span>"
-    + "</a>"
-+ "<nav class='site-nav'>"
-    + "<a href='#' class='menu-icon'>"
-        + "<div class='trigger'>"
-            + "<a class='nav-link' href='/'>Home</a>"
-            + "<a class='nav-link' href='/boats'>Boats</a>"
-            + "<a class='nav-link' href='/team'>Team</a>"
-            + "<a class='nav-link' href='/sponsor'>Sponsor</a>"
-            + "<a class='nav-link' href='/contact'>Contact</a>"
-            + "<a class='nav-link' href='/join'>Join</a>"
-        + "</div>"
-    + "</nav>"
-+ "</div>"
+function loadHeader() {
+    document.getElementById("umeb-header").innerHTML =
+    "<div class='wrapper'>"
+        + "<a class='site-title' href='/'>"
+            + "<span class='site-logo'>"
+                + "<img src='/assets/img/logo.png'>"
+            + "</span>"
+        + "</a>"
+    + "<nav class='site-nav'>"
+        + "<a href='#' class='menu-icon'>"
+            + "<div class='trigger'>"
+                + "<a class='nav-link' href='/'>Home</a>"
+                + "<a class='nav-link' href='/boats'>Boats</a>"
+                + "<a class='nav-link' href='/team'>Team</a>"
+                + "<a class='nav-link' href='/sponsor'>Sponsor</a>"
+                + "<a class='nav-link' href='/contact'>Contact</a>"
+                + "<a class='nav-link' href='/join'>Join</a>"
+            + "</div>"
+        + "</nav>"
+    + "</div>"
+}
 
 /*********************
 SITE FOOTER
 *********************/
-
-document.addEventListener('DOMContentLoaded', function() {
-    loadFooter();
-});
 
 function loadFooter() {
     const footer = document.getElementById('umeb-footer');
@@ -81,4 +85,30 @@ function parseSocialCSV(data) {
         const [name, path, link] = row.split(',');
         return { name, imagePath: path, link };
     });
+}
+
+/*********************
+SITE HEROS
+*********************/
+
+function loadHeros() {
+    const heros = { 'main-hero'     : '/assets/img/heros/main.jpg',
+                    'boats-hero'    : '/assets/img/heros/boats.jpg',
+                    'team-hero'     : '/assets/img/heros/team.jpg',
+                    'sponsor-hero'  : '/assets/img/heros/sponsor.jpg',
+                    'contact-hero'  : '/assets/img/heros/contact.jpg',
+                    'join-hero'     : '/assets/img/heros/join.jpg'};
+    
+    for (const [elementId, imagePath] of Object.entries(heros)) {
+        const heroContainer = document.getElementById(elementId);
+        if (heroContainer) {
+            heroContainer.style.backgroundImage = `url('${imagePath}')`;
+            heroContainer.innerHTML =
+            `<div class='wrap'>`
+                + `<div class='hero-content' style='height: 400px'></div>`
+            + `</div>`
+        } else {
+            console.error(`Could not find hero for id: ${elementId}`)
+        }
+    }
 }
